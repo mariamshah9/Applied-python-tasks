cafa24c9f9a14af129ecfd538b0095ef0b38e608
mariamshah9 <80720886+mariamshah9@users.noreply.github.com>
2024-02-09 20:23:48 +0530

    Update permutations.py

--- programs/hackerrank/permutations.py
+++ programs/hackerrank/permutations.py
@@ -1,3 +1,13 @@
+#Task: You are given a string S.Your task is to print all possible permutations of size
+of the string in lexicographic sorted order.
+    
+Input Format:A single line containing the space separated string
+and the integer value
+
+Constraints:The string contains only UPPERCASE characters.
+Output Format:Print the permutations of the string on separate lines.
+
+
 from itertools import permutations
 str1, int1 = input().split()
 
